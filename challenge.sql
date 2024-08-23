bd91b7343f9fff4e9ae49040fd41cd938d411721
JetJagar <ce.mini1991@yahoo.com>
2024-08-22 20:28:57 -0500

    query on

--- challenge.sql
+++ challenge.sql
@@ -34,3 +34,33 @@ CREATE TABLE titles (
   title_id character varying(50) NOT NULL,
   title character varying(50) NOT NULL
 );
+
+-- SQL queries challenge starts here
+
+-- List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s
+ON (e.emp_no = s.emp_no)
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT dm.dept_no AS department_number, d.dept_name AS department_name, 
+        e.emp_no AS employee_number, e.first_name, e.last_name
+FROM dept_manager dm
+JOIN departments d ON dm.dept_no = d.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+-- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT de.dept_no AS department_number, d.dept_name AS department_name, 
+		e.emp_no AS employee_number, e.first_name, e.last_name
+FROM dept_emp as de
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN employees e ON de.emp_no = e.emp_no;
+
+
+
